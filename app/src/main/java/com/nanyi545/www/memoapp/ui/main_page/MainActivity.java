ce524c757934ca128a0385568699ca2c9c8fca8e
Your Name <nanyi545@126.com>
2018-03-13 16:02:16 +0800

    binder

--- app/src/main/java/com/nanyi545/www/memoapp/ui/main_page/MainActivity.java
+++ app/src/main/java/com/nanyi545/www/memoapp/ui/main_page/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mainRv= (RecyclerView) findViewById(R.id.main_rv);
         mainRv.setLayoutManager(new LinearLayoutManager(this));
 
